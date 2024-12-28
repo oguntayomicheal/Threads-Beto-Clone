40518334d2b4cbbcff730676e0af97d8efdd2688
oguntayo micheal <oguntayomicheal1@gmail.com>
2024-12-28 18:18:02 +0100

    Add context for state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { ThreadProvdier } from '@/context/thread-context';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -50,10 +51,14 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <ThreadProvdier>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+
+      </ThreadProvdier>
+
     </ThemeProvider>
   );
 }
